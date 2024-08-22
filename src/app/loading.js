352133897b13669bef638494381fb7a152d82541
Kanishka2301 <kanishkaskumar2004@gmail.com>
2024-08-22 19:21:28 +0530

    initial commit

---
+++ src/app/loading.js
@@ -0,0 +1,9 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function loading() {
+  return (
+    <div className="w-full bg-black min-h-screen">
+      <Skeleton />
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
       <h1>Welcome to recipe app</h1>
+      <Link href={"/recipe-list"}>Explore recipes</Link>
     </div>
   );
 }
